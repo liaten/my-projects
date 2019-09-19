9393d96168743bee6ec913adce33a6fbd233d702
liaten <35489460+liaten@users.noreply.github.com>
2019-09-19 14:41:20 +0100

    Add files via upload

---
+++ houses and lines/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter N: ";int n;cin>>n;
+    int sq[n][n];
+    sq[0][0]=1;
+    for(int i=0;i<n;i++){
+        for(int f=0;f<n;f++){
+            sq[];
+        }
+    }
+    return 0;
+}
