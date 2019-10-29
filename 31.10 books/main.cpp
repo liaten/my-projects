7f3e79c659534e94000680a2f56303ff56fb2b5a
liaten <vlad171vlad@gmail.com>
2019-10-30 01:17:00 +0300

    начал делать прогу для библиотеки

--- 31.10 books/main.cpp
+++ 31.10 books/main.cpp
@@ -1,30 +1,46 @@
 #include <iostream>     //ввод вывод
-#include <clocale>      //русский язык
-#include <fstream>      //для работы с текстом
-#include <cstring>      //проверяет длину строки
+#include <clocale>
+#include <fstream>      //для работы с текстовыми файлами
+//#include <cstring>      //проверяет длину строки
+#include <string>
+#include <map>
+//#include <windows.h>    //библа винды
+
 using namespace std;    //ввод вывод
+struct library
+{
+    char *author;   // Фамилия автора книги
+    char *title;    // Заголовок
+    char *firm;     // Издательство
+    int year;       // Год издания
+    int pages;      // Количество страниц
+    int value;      // Стоимость
+};
 
 int main()
 {
     setlocale(LC_ALL,"Rus");
-    ifstream in;
+    /*SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);*/
+    ifstream in;    // Поток ввода
     in.open ("input.txt",ios::in);
-    //short UPP1=3;
-    short UPP;
-    int COUNT;
-    int COUNT2;
+    short STRCOUNT=1;   // Счетчик строк для внесения в структуру
     if(in.is_open())
     {
-        //cout<<"Файл ввода открылся\n";
-        ofstream out;          // поток для записи
+        cout<<"Файл ввода открылся\n";
+        ofstream out;          // Поток вывода
         out.open("output.txt", ios::trunc);
         if(out.is_open())
         {
-            //cout<<"Файл вывода открылся\n";
+            cout<<"Файл вывода открылся\n";
             string s;
-            while (getline(in,s))
+            while (getline(in,s)) STRCOUNT++;
+            STRCOUNT/=6;
+            map<string,library>vars;
+            for(int i=1;i<STRCOUNT+1;++i)
             {
-                
+                vars["Book"+to_string(i)];
+                cout<<"Book"+to_string(i)<<endl;
             }
         }
         else cout<<"Файл вывода не открылся\n";
