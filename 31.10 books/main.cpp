25a2ec0cca67b0cfcd678a2391672214c9191ac6
liaten <vlad171vlad@gmail.com>
2019-10-30 20:28:22 +0300

    работа структуры

--- 31.10 books/main.cpp
+++ 31.10 books/main.cpp
@@ -2,16 +2,18 @@
 #include <clocale>
 #include <fstream>      //для работы с текстовыми файлами
 //#include <cstring>      //проверяет длину строки
-#include <string>
+#include <sstream>
+//#include <string>
 #include <map>
 //#include <windows.h>    //библа винды
+//#include <math.h>
 
 using namespace std;    //ввод вывод
 struct library
 {
-    char *author;   // Фамилия автора книги
-    char *title;    // Заголовок
-    char *firm;     // Издательство
+    string author;   // Фамилия автора книги
+    string title;    // Заголовок
+    string firm;     // Издательство
     int year;       // Год издания
     int pages;      // Количество страниц
     int value;      // Стоимость
@@ -24,24 +26,59 @@ int main()
     SetConsoleOutputCP(1251);*/
     ifstream in;    // Поток ввода
     in.open ("input.txt",ios::in);
-    short STRCOUNT=1;   // Счетчик строк для внесения в структуру
+    int STRCOUNT=1;     // Счетчик строк для внесения в структуру
+    int BOOKNUM=0;      // Счетчик книг
     if(in.is_open())
     {
-        cout<<"Файл ввода открылся\n";
+        //cout<<"Файл ввода открылся\n";
         ofstream out;          // Поток вывода
         out.open("output.txt", ios::trunc);
         if(out.is_open())
         {
-            cout<<"Файл вывода открылся\n";
+            //cout<<"Файл вывода открылся\n";
             string s;
-            while (getline(in,s)) STRCOUNT++;
-            STRCOUNT/=6;
+            /*while (getline(in,s)) STRCOUNT++;
+            STRCOUNT/=6;*/
             map<string,library>vars;
-            for(int i=1;i<STRCOUNT+1;++i)
+            /*for(int i=1;i<STRCOUNT+1;++i)
             {
                 vars["Book"+to_string(i)];
                 cout<<"Book"+to_string(i)<<endl;
+            }*/
+            in.close();
+            in.open ("input.txt",ios::in);
+            STRCOUNT=0;
+            while (getline(in,s))
+            {
+                STRCOUNT++;
+                if(STRCOUNT%6!=0) BOOKNUM = (STRCOUNT/6)+1;
+                else BOOKNUM = STRCOUNT/6;
+                vars["Book"+to_string(BOOKNUM)];
+                //cout<<"Book"+to_string(BOOKNUM)<<" "<<STRCOUNT<<endl;
+                if(STRCOUNT%6==1) vars["Book"+to_string(BOOKNUM)].author    = s;
+                if(STRCOUNT%6==2) vars["Book"+to_string(BOOKNUM)].title     = s;
+                if(STRCOUNT%6==3) vars["Book"+to_string(BOOKNUM)].firm      = s;
+                if(STRCOUNT%6==4)
+                {
+                    istringstream iss (s, istringstream::in);
+                    iss>>vars["Book"+to_string(BOOKNUM)].year;
+                }
+                if(STRCOUNT%6==5)
+                {
+                    istringstream iss (s, istringstream::in);
+                    iss>>vars["Book"+to_string(BOOKNUM)].pages;
+                }
+                if(STRCOUNT%6==0)
+                {
+                    istringstream iss (s, istringstream::in);
+                    iss>>vars["Book"+to_string(BOOKNUM)].value;
+                }
+                /*  тут проверка на работу структуры
+                    cout<<vars["Book"+to_string(BOOKNUM)].author<<" "<<vars["Book"+to_string(BOOKNUM)].title<<" "<<vars["Book"+to_string(BOOKNUM)].firm<<" ";
+                    cout<<vars["Book"+to_string(BOOKNUM)].year<<" "<<vars["Book"+to_string(BOOKNUM)].pages<<" "<<vars["Book"+to_string(BOOKNUM)].value<<endl;
+                */
             }
+            //ясно автор ушел спать
         }
         else cout<<"Файл вывода не открылся\n";
     }
