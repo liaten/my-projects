efe2f40261c650f203ad9bb9c1b9fc3f08e81930
liaten <vlad171vlad@gmail.com>
2019-11-10 23:23:09 +0300

    доработал треугольники

--- 14.11 Triangles linux/main.cpp
+++ 14.11 Triangles linux/main.cpp
@@ -1,21 +1,25 @@
-#include <stdlib.h>
-#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdio.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int TRSIZE;
-    cin>>TRSIZE;
+    int TRSIZE, xput;
+    cout<<"Enter number of triangles: ";
+    cin>>TRSIZE;        //допустим 3
     int maxlen=TRSIZE*2;
-    for(int i=1;i<TRSIZE+1;i++)
+    for(int i=1;i<TRSIZE+1;i++) //построчный вывод
     {
-        for(int j=1;j<TRSIZE+1;j++)
+        xput=i*2-1;
+        //cout<<TRSIZE-(xput-i)<<endl;
+        //cout<<TRSIZE+(xput-i)<<endl;
+        for(int j=1;j<TRSIZE+1;j++) //
         {
             for(int k=1;k<maxlen+1;k++)
             {
-                putchar('O');
-                putchar(' ');
+                if(k>TRSIZE-(xput-i+1) && k<TRSIZE+(xput-i+1)) putchar('*');
+                else putchar(' ');
             }
         }
         putchar('\n');
