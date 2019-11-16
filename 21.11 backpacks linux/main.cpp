32a92fe5bbf0f7f8ca19c27e64786af07d0cf207
liaten <vlad171vlad@gmail.com>
2019-11-16 22:56:00 +0300

    backpacks idea improvements and changes for 2 backpacks

--- 21.11 backpacks linux/main.cpp
+++ 21.11 backpacks linux/main.cpp
@@ -3,8 +3,33 @@ using namespace std;
 
 int main()
 {
-    //cout << "Программа предназначена для распределения вещей разной массы по N рюкзакам\n";
-    int BNUM;
-    
+    int tCOUNT; //счетчик вещей
+    int bCOUNT; //счетчик рюкзаков
+    cout<<"Введите количество рюкзаков: "; cin>>bCOUNT;
+    cout<<"Введите количество вещей: "; cin>>tCOUNT;
+    int s[tCOUNT];
+    for(int i=0;i<tCOUNT;i++)
+    {
+        cout<<"Введите массу вещи "<<i+1<<" :";
+        cin>>s[i];
+    }
+    for (int i = 0; i < tCOUNT; i++)
+    {
+        for (int n = 0; n < tCOUNT; n++)
+        {
+            if (s[n] > s[n+1])
+            {
+                s[tCOUNT] = s[n];
+                s[n] = s[n + 1];
+                s[n + 1] = s[tCOUNT];
+            }
+        }
+    }
+    cout<<"Все вещи в порядке возрастания выглядят так: ";
+    for (int i = 0; i < tCOUNT; i++)
+    {
+        cout<<'['<<s[i]<<']';
+    }
+    putchar('\n');
     return 0;
 }
