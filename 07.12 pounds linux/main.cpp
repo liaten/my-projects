eaf49fa646c0979c6c5348cb29824abdb2f92a80
liaten <vlad171vlad@gmail.com>
2019-12-06 22:39:08 +0300

    012***6

--- 07.12 pounds linux/main.cpp
+++ 07.12 pounds linux/main.cpp
@@ -6,10 +6,11 @@ using namespace std;
 struct eng_money
 {
 public:
+//012***6
 //  0)Ввод
-    void enter(eng_money,unsigned i)
+    void enter(eng_money,unsigned &i)
     {
-        cout<<"Введите данные кошелька №"<<i<<endl;
+        cout<<"Введите данные денежной суммы №"<<i<<'\n';
         cout<<"Фунты: ";
         cin>>pounds;
         cout<<"Шиллинги: ";
@@ -17,12 +18,56 @@ public:
         cout<<"Пенсы: ";
         cin>>pence;
     }
-//  1)Проверка корректности значения
-    bool correct_value(eng_money)
+//  6)Вывод
+    void output(eng_money,unsigned &i)
     {
-        if(shillings>=20) return 0;
-        if(pence>=12) return 0;
-        return 1;
+        cout<<"Денежная сумма №"<<i<<": <<"<<pounds<<'-'<<shillings<<'-'<<pence<<">>\n";
+    }
+//  1)Проверка корректности значения
+    void correct_value(eng_money)
+    {
+        if(pence>=12)
+        {
+            shillings+=(pence/12);
+            pence%=12;
+        }
+        if(shillings>=20)
+        {
+            pounds+=(shillings/20);
+            shillings%=20;
+        }
+    }
+//  2)Увеличение заданной суммы
+    void increase(eng_money,unsigned &i)
+    {
+        unsigned pounds_increment, shillings_increment, pence_increment;
+        cout<<"На сколько хотите увеличить денежную сумму №"<<i<<"? \nФунты: ";
+        cin>>pounds_increment;
+        cout<<"Шиллинги: ";
+        cin>>shillings_increment;
+        cout<<"Пенсы: ";
+        cin>>pence_increment;
+        if(pounds_increment!=0)
+        {
+            shillings_increment+=(pounds_increment*20);
+            pounds_increment=0;
+        }
+        if(shillings_increment!=0)
+        {
+            pence_increment+=(shillings_increment*12);
+            shillings_increment=0;
+        }
+        if(pounds!=0)
+        {
+            shillings+=(pounds*20);
+            pounds=0;
+        }
+        if(shillings!=0)
+        {
+            pence+=(shillings*12);
+            shillings=0;
+        }
+        pence+=pence_increment;
     }
 private:
     unsigned pounds;    //фунты
@@ -37,9 +82,11 @@ int main()
     cin>>N;
     for(unsigned i=1;i<N+1;i++)
     {
-        purse[to_string(i)].enter(purse[to_string(i)],i);
-        cout<<purse[to_string(i)].correct_value(purse[to_string(i)])<<endl; //проверка на корректность значения
-    }
-    cout<<"Выберите действие:\n1)Проверка корректности значения\n2)Увеличение заданной суммы\n3)Сложение двух денежных сумм\n4)Вычисление разности денежных сумм\n5)Перевод денежной суммы в пенсы\n6)Вывод денежной суммы в формате <<99-99-99>>\n";
-    return 0;
+        purse[to_string(i)].enter(purse[to_string(i)],i);       //ввод
+        purse[to_string(i)].correct_value(purse[to_string(i)]); //проверка и перевод
+        purse[to_string(i)].increase(purse[to_string(i)],i);    //увеличение
+        purse[to_string(i)].correct_value(purse[to_string(i)]); //проверка и перевод
+        purse[to_string(i)].output(purse[to_string(i)],i);      //вывод
+
+    }return 0;
 }
